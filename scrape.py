726a7a08f7fc7884a2c1242c7dbfa8a30dc38d4b
lzwjava <lzwjava@gmail.com>
2021-03-17 16:57:15 +0800

    a

--- scrape.py
+++ scrape.py
@@ -1,10 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
+from multiprocessing import Process
+import timeit
 
 def scrape(chapter):
     if chapter < 1 or chapter > 52:
         raise Exception(f'chapter {chapter}')
-    chapter_str = '%2d'.format(chapter)
+    chapter_str = '{:02d}'.format(chapter)
     url = f'https://www.feynmanlectures.caltech.edu/I_{chapter_str}.html'
     print(f'scraping {url}')
     r = requests.get(url)
@@ -16,7 +18,14 @@ def scrape(chapter):
     f.close()
 
 def main():
-    for i in range(52):
-        scrape(i+1)
-        
-main()
+    start = timeit.default_timer()
+    ps = [Process(target=scrape, args=(i+1,)) for i in range(52)]
+    for p in ps:
+        p.start()
+    for p in ps:
+        p.join()
+    stop = timeit.default_timer()
+    print('Time: ', stop - start) 
+
+if __name__ == "__main__":    
+    main()

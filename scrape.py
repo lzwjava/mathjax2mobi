c0cfc4126d66fe313cbcde0a65a46b292c4c5f10
lzwjava <lzwjava@gmail.com>
2021-03-17 17:41:28 +0800

    a

--- scrape.py
+++ scrape.py
@@ -2,24 +2,26 @@ import requests
 from bs4 import BeautifulSoup
 from multiprocessing import Process
 import timeit
+from pathlib import Path
 
 def scrape(chapter):
     if chapter < 1 or chapter > 52:
         raise Exception(f'chapter {chapter}')
     chapter_str = '{:02d}'.format(chapter)
+    Path(f'./chapters/{chapter_str}').mkdir(parents=True, exist_ok=True)
     url = f'https://www.feynmanlectures.caltech.edu/I_{chapter_str}.html'
     print(f'scraping {url}')
     r = requests.get(url)
     if r.status_code != 200:
         raise Exception(r.status_code)
     soup = BeautifulSoup(r.text, features='lxml')
-    f = open(f'./chapters/I_{chapter_str}.html', 'w')
+    f = open(f'./chapters/{chapter_str}/I_{chapter_str}.html', 'w')
     f.write(soup.prettify())
     f.close()
 
 def main():
     start = timeit.default_timer()
-    ps = [Process(target=scrape, args=(i+1,)) for i in range(52)]
+    ps = [Process(target=scrape, args=(i+1,)) for i in range(1)]
     for p in ps:
         p.start()
     for p in ps:

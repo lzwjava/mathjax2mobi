249620ac512f7a811fc0403f44d871b5318d5203
lzwjava <lzwjava@gmail.com>
2021-03-17 23:10:25 +0800

    a

--- feynman.py
+++ feynman.py
@@ -1,8 +1,10 @@
 import subprocess
+from sys import prefix
 from bs4 import BeautifulSoup
 from bs4.element import PageElement
 from latex2svg import latex2svg
 from pathlib import Path
+from multiprocessing import Process
 
 def clean_mathjax(soup, name, cls):
     previews = soup.find_all(name, {'class': cls})
@@ -14,12 +16,12 @@ def clean_script(soup):
     for s in scripts:
         s.decompose()    
 
-def wrap_latex(mathjax, equation = False):
+def wrap_latex(latex, equation = False):
     wrap = ''
     if equation:
-        wrap = mathjax.string
+        wrap = latex.string
     else:
-        wrap = '$' + mathjax.string + '$'
+        wrap = '$' + latex.string + '$'
     wrap = wrap.replace('label', 'tag')
     return wrap
  
@@ -33,36 +35,54 @@ def wrap_svg(svg, equation):
     
 def svg_last_dir(svg_path):
     splits = svg_path.split('/')
-    return splits[len(splits) - 1]        
+    return splits[len(splits) - 1]    
 
-def to_svg(mathjaxs: [PageElement], svg_path: str, equation=False):
+def svg_prefix(equation: bool):
+    prefix = ''
     if equation:
-        svg_prefix = 'eq_'
+        prefix = 'eq_'
     else:
-        svg_prefix = 'in_'
-    svg_i = 0
-    for mathjax in mathjaxs:     
-        print(mathjax.string)
-        wrap = wrap_latex(mathjax, equation=equation)
-        out = {}
-        try:
-            out = latex2svg(wrap)   
-        except subprocess.CalledProcessError as err:
-            raise err      
-            
-        f = open(f'{svg_path}/{svg_prefix}{svg_i}.svg', 'w')
-        f.write(out['svg'])
-        f.close()
+        prefix = 'in_'
+    return prefix
+
+def make_svg(latex_str: str, svg_path: str, svg_i: int, equation: bool):
+    prefix = svg_prefix(equation)
+    out = {}
+    try:
+        out = latex2svg(latex_str)   
+    except subprocess.CalledProcessError as err:
+        raise err      
         
-        node = BeautifulSoup('<img>', features="lxml")
-        img = node.find('img')
-        img.attrs['src'] = f'{svg_last_dir(svg_path)}/{svg_prefix}{svg_i}.svg'
-        img.attrs['style'] = 'vertical-align: middle; margin: 0.5em 0;'
+    f = open(f'{svg_path}/{prefix}{svg_i}.svg', 'w')
+    f.write(out['svg'])
+    f.close()    
+
+def insert_svg(latex: PageElement, svg_path: str, svg_i: int, equation: bool):
+    prefix = svg_prefix(equation)
+    
+    node = BeautifulSoup('<img>', features="lxml")
+    img = node.find('img')
+    img.attrs['src'] = f'{svg_last_dir(svg_path)}/{prefix}{svg_i}.svg'
+    img.attrs['style'] = 'vertical-align: middle; margin: 0.5em 0;'
+    
+    p = wrap_svg(img, equation)
+    latex.insert_after(p)
+
+def to_svg(latexs: [PageElement], svg_path: str, equation=False):
+    ps = []
+    for (svg_i, latex) in enumerate(latexs):  
+        print(latex.string)
+        latex_str = wrap_latex(latex, equation)
         
-        p = wrap_svg(img, equation)
-        mathjax.insert_after(p)
+        p = Process(target=make_svg, args=(latex_str, svg_path, svg_i, equation))
+        ps.append(p)
+    for p in ps:
+        p.start()
+    for p in ps:
+        p.join()
         
-        svg_i +=1
+    # for (svg_i, latex) in enumerate(latexs):  
+    #     insert_svg(latex, svg_path, svg_i, equation)
         
 def mathjax2svg(source: str, svg_path: str) -> str:
     Path(svg_path).mkdir(parents=True, exist_ok=True)    
@@ -72,11 +92,11 @@ def mathjax2svg(source: str, svg_path: str) -> str:
     clean_mathjax(soup, 'div', 'MathJax_Display')
     clean_mathjax(soup, 'span', 'MathJax_Preview')
     
-    mathjaxs = soup.find_all('script', {'type': 'math/tex'})
-    to_svg(mathjaxs, svg_path, equation=False)
+    latexs = soup.find_all('script', {'type': 'math/tex'})
+    to_svg(latexs, svg_path, equation=False)
     
-    mathjaxs = soup.find_all('script', {'type': 'math/tex; mode=display'})   
-    to_svg(mathjaxs, svg_path, equation=True)
+    latexs = soup.find_all('script', {'type': 'math/tex; mode=display'})   
+    to_svg(latexs, svg_path, equation=True)
     
     clean_script(soup)  
     return soup.prettify()      

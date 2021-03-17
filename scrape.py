98f13ce744f2a5c4894add60a966559bfa592629
lzwjava <lzwjava@gmail.com>
2021-03-17 16:26:56 +0800

    a

--- scrape.py
+++ scrape.py
@@ -1,13 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
 
-def main():
-    r = requests.get('https://www.feynmanlectures.caltech.edu/I_toc.html')
+def scrape(chapter):
+    if chapter < 1 or chapter > 52:
+        raise Exception(f'chapter {chapter}')
+    chapter_str = '%2d'.format(chapter)
+    url = f'https://www.feynmanlectures.caltech.edu/I_{chapter_str}.html'
+    print(f'scraping {url}')
+    r = requests.get(url)
     if r.status_code != 200:
-        return
+        raise Exception(r.status_code)
     soup = BeautifulSoup(r.text, features='lxml')
-    f = open('toc.html', 'w')
+    f = open(f'./chapters/I_{chapter_str}.html', 'w')
     f.write(soup.prettify())
     f.close()
 
+def main():
+    for i in range(52):
+        scrape(i+1)
+        
 main()

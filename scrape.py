fcad03498c91e7df4ec2cf6f818a27cc51a0b6d9
lzwjava <lzwjava@gmail.com>
2021-03-17 16:11:37 +0800

    a

--- scrape.py
+++ scrape.py
@@ -1,7 +1,13 @@
 import requests
+from bs4 import BeautifulSoup
 
 def main():
-    r = requests.get('https://api.github.com/user', auth=('user', 'pass'))
-    print(r.status_code)
+    r = requests.get('https://www.feynmanlectures.caltech.edu/I_toc.html')
+    if r.status_code != 200:
+        return
+    soup = BeautifulSoup(r.text, features='lxml')
+    f = open('toc.html', 'w')
+    f.write(soup.prettify())
+    f.close()
 
 main()
